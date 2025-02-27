ee48c02402b459d6464d86698a31d6d70b2614ae
Olga Budytska <olgabudickaja@gmail.com>
2025-02-27 08:18:19 +0200

    fixed task_1

--- task_1.py
+++ task_1.py
@@ -11,14 +11,15 @@ def init_db():
     add_values('продукти', 980)
     add_values('канцелярія', 3470)
     add_values('одяг', 35000)
-
+    
 def commit_decorator(func):
-    def wrapper(func, *args, **kwargs):
+    def wrapper(*args, **kwargs):
         query = func(*args, **kwargs)
         cursor.execute(query)
         connection.commit()
     return wrapper
 
+@commit_decorator
 def create_table():
     query = '''CREATE TABLE IF NOT EXISTS Costs (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -29,8 +30,14 @@ def create_table():
     return query
 
 def add_values(purpose:str, sum:int):
-    query = '''INSERT INTO Costs (purpose, sum) VALUES (?, ?)'''
-    cursor.execute(query, (purpose, sum))
-    connection.commit()
+     if table_exists("Costs"):
+        query = '''INSERT INTO Costs (purpose, sum) VALUES (?, ?)'''
+        cursor.execute(query, (purpose, sum))
+        connection.commit()
+
+def table_exists(table_name: str) -> bool:
+    query = '''SELECT name FROM sqlite_master WHERE type='table' AND name=?'''
+    cursor.execute(query, (table_name,))
+    return cursor.fetchone() is not None
 
 init_db()
